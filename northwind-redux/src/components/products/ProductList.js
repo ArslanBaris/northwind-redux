8ce7eeb97146ad86020db065209ee792a0cd1e2e
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-22 17:16:05 +0300

    Add to cart transaction.

--- northwind-redux/src/components/products/ProductList.js
+++ northwind-redux/src/components/products/ProductList.js
@@ -1,46 +1,54 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Badge, Table } from "reactstrap";
+import { Badge, Button, Table } from "reactstrap";
 import { bindActionCreators } from "redux";
 import * as productActions from "../../redux/actions/productAction";
+import * as cartActions from "../../redux/actions/cartActions";
+import alertify from "alertifyjs"
 
 class ProductList extends Component {
-  componentDidMount(){
-    this.props.actions.getProducts()
+  componentDidMount() {
+    this.props.actions.getProducts();
+  }
+
+  addToCart=(product)=>{
+    this.props.actions.addToCart({quantity:1,product})
+    alertify.success(product.productName+" added to cart.")
   }
 
   render() {
     return (
       <div>
-        
+        <h3>
           <Badge color="warning">Products</Badge>{" "}
-          <Badge  color="success">{this.props.currentCategory.categoryName}</Badge>
-          <Table>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Product Name</th>
-                <th>Unit Price</th>
-                <th>Quantity Per Unit</th>
-                <th>Units In Stock</th>
-              </tr>
-            </thead>
-            <tbody>
-            {
-              this.props.products.map(product=>(
-                <tr key = {product.id}>
+          <Badge color="success">
+            {this.props.currentCategory.categoryName}
+          </Badge>
+        </h3>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product Name</th>
+              <th>Unit Price</th>
+              <th>Quantity Per Unit</th>
+              <th>Units In Stock</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.products.map((product) => (
+              <tr key={product.id}>
                 <th>{product.id}</th>
                 <td>{product.productName}</td>
                 <td>{product.unitPrice}</td>
                 <td>{product.quantityPerUnit}</td>
                 <td>{product.unitsInStock}</td>
+                <td><Button color="success" onClick={()=>{this.addToCart(product)}}>Add</Button></td>
               </tr>
-              ))
-            }
-            
-            </tbody>
-          </Table>
-        
+            ))}
+          </tbody>
+        </Table>
       </div>
     );
   }
@@ -49,19 +57,17 @@ class ProductList extends Component {
 function mapStateToProps(state) {
   return {
     currentCategory: state.changeCategoryReducer,
-    products : state.productListReducer,
+    products: state.productListReducer,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     actions: {
-      getProducts: bindActionCreators(
-        productActions.getProducts,
-        dispatch
-      ),
+      getProducts: bindActionCreators(productActions.getProducts, dispatch),
+      addToCart : bindActionCreators(cartActions.addToCart, dispatch),
 
     },
   };
 }
-export default connect(mapStateToProps,mapDispatchToProps)(ProductList);
+export default connect(mapStateToProps, mapDispatchToProps)(ProductList);
