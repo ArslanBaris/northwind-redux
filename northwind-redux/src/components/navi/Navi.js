1ff1044ba372388a03a8167b52d431d9f2a74f0c
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-22 20:54:18 +0300

    Remove from cart transaction

--- northwind-redux/src/components/navi/Navi.js
+++ northwind-redux/src/components/navi/Navi.js
@@ -6,13 +6,9 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
+  NavLink
 } from 'reactstrap';
+import CartSummary from '../cart/CartSummary';
 
 const Navi = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -25,32 +21,15 @@ const Navi = (props) => {
         <NavbarBrand href="/">reactstrap</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
+          <Nav className="ml-auto" navbar>
             <NavItem>
               <NavLink href="/components/">Components</NavLink>
             </NavItem>
             <NavItem>
               <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
             </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
+            <CartSummary />
           </Nav>
-          <NavbarText>Simple Text</NavbarText>
         </Collapse>
       </Navbar>
     </div>
--- northwind-redux/src/redux/reducers/cartReducer.js
+++ northwind-redux/src/redux/reducers/cartReducer.js
@@ -7,7 +7,7 @@ export default function cartReducer(state = initialState.cart, action) {
         var addedItem = state.find(c=>c.product.id===action.payload.product.id)
         if(addedItem){
             var newState = state.map(cartItem=>{
-                if(cartItem.product.id===action.cartItem.product.id){
+                if(cartItem.product.id===action.payload.product.id){
                     return Object.assign({},addedItem,{quantity:addedItem.quantity+1})
                 }
                 return cartItem;
@@ -19,7 +19,8 @@ export default function cartReducer(state = initialState.cart, action) {
       return action.payload;
 
     case actionTypes.REMOVE_FROM_CART:
-      return action.payload;
+      const newState2 = state.filter(cartItem => cartItem.product.id!==action.payload.id)
+      return newState2;
 
     default:
       return state;
